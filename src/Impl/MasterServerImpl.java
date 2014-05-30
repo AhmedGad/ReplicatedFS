51c574648d53db72ca6758a6ce24aabfed977e86
ahmedGad <ahmed.mu.gad@gmail.com>
2014-05-31 02:27:12 +0300

    almost finished - spaghetti code - no client yet - no testing yet - worst commit ever

--- src/Impl/MasterServerImpl.java
+++ src/Impl/MasterServerImpl.java
@@ -1,13 +1,21 @@
 package Impl;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.Random;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Lock;
@@ -22,8 +30,8 @@ public class MasterServerImpl implements MasterServerClientInterface {
 	private ConcurrentHashMap<String, Lock> fileLock;
 	private ReplicaLoc[] replicaServerAddresses;
 	private AtomicInteger txnID, timeStamp;
-	private String dir;
 	private static int NUM_REPLICA_PER_FILE = 2;
+	FileWriter metaDataWriter;
 
 	private static Random r = new Random(System.nanoTime());
 
@@ -31,11 +39,31 @@ public class MasterServerImpl implements MasterServerClientInterface {
 		return r.nextInt();
 	}
 
-	public MasterServerImpl(String dir, File metaData, File repServers) {
+	public MasterServerImpl(File metaData,
+			TreeMap<String, ReplicaLoc> nameToLocMap) throws IOException {
 		locMap = new ConcurrentHashMap<String, ReplicaLoc[]>();
 		txnID = new AtomicInteger(0);
 		timeStamp = new AtomicInteger(0);
-		this.dir = dir;
+		replicaServerAddresses = new ReplicaLoc[nameToLocMap.size()];
+
+		int ii = 0;
+		for (ReplicaLoc loc : nameToLocMap.values()) {
+			replicaServerAddresses[ii++] = loc;
+		}
+
+		Scanner scanner = new Scanner(metaData);
+		while (scanner.hasNext()) {
+			StringTokenizer tok = new StringTokenizer(scanner.nextLine());
+			String fName = tok.nextToken();
+			ReplicaLoc[] fileLocations = new ReplicaLoc[tok.countTokens()];
+			for (int i = 0; i < fileLocations.length; i++) {
+				fileLocations[i] = nameToLocMap.get(fName);
+			}
+			locMap.put(fName, fileLocations);
+		}
+		scanner.close();
+
+		metaDataWriter = new FileWriter(metaData, true);
 	}
 
 	@Override
@@ -60,52 +88,143 @@ public class MasterServerImpl implements MasterServerClientInterface {
 	@Override
 	public WriteMsg write(FileContent data) throws RemoteException, IOException {
 		String fileName = data.getFileName();
-		// This step guarantees that clients who request same file reach out the
-		// primary replica in the order which they obtain their transaction id's
-		Lock lock = new ReentrantLock(true);
-		try {
-			(lock = fileLock.putIfAbsent(fileName, lock)).lock();
-			int tId = txnID.incrementAndGet();
-			int ts = timeStamp.incrementAndGet();
-			ReplicaLoc[] locations = null;
-			if (locMap.containsKey(fileName)) {
-				locations = locMap.get(fileName);
-			} else {
-				locations = selectRandomReplicas();
+
+		// check if this is a commit acknowledgment
+		if (data.getData() == null) {
+			synchronized (this) {
+				ReplicaLoc[] locations = locMap.get(fileName);
+				metaDataWriter.write(fileName);
+				for (int i = 0; i < locations.length; i++) {
+					metaDataWriter.write(" " + locations[i].getName());
+				}
+				metaDataWriter.write("\n");
+				metaDataWriter.flush();
 			}
-			ReplicaLoc primary = locations[0];
-			ReplicaServerClientInterface primaryServer = null;
+			return null;
+		} else {
+			// This step guarantees that clients who request same file reach out
+			// the
+			// primary replica in the order which they obtain their transaction
+			// id's
+			Lock lock = new ReentrantLock(true);
 			try {
-				primaryServer = (ReplicaServerClientInterface) LocateRegistry
-						.getRegistry(primary.getHost(), primary.getPort())
-						.lookup(primary.getName());
-			} catch (Exception e) {
+				(lock = fileLock.putIfAbsent(fileName, lock)).lock();
+				int tId = txnID.incrementAndGet();
+				int ts = timeStamp.incrementAndGet();
+				ReplicaLoc[] locations = null;
+				if (locMap.containsKey(fileName)) {
+					locations = locMap.get(fileName);
+				} else {
+					locations = selectRandomReplicas();
+				}
+				ReplicaLoc primary = locations[0];
+				ReplicaServerClientInterface primaryServer = null;
+				try {
+					primaryServer = (ReplicaServerClientInterface) LocateRegistry
+							.getRegistry(primary.getHost(), primary.getPort())
+							.lookup(primary.getName());
+				} catch (Exception e) {
+				}
+				primaryServer.write(tId, 1, data);
+				return new WriteMsg(tId, ts, primary);
+			} finally {
+				lock.unlock();
 			}
-			primaryServer.write(tId, 1, data);
-			return new WriteMsg(tId, ts, primary);
-		} finally {
-			lock.unlock();
 		}
 	}
 
-	public static void main(String[] args) {
-		int port = 1099;
-		String dir = "./FS";
-		File metaData = new File("metaData.txt");
-		File repServers = new File("repServers.txt");
+	static class StreamReader implements Runnable {
 
-		try {
-			Registry registry = LocateRegistry.createRegistry(port);
-			MasterServerImpl masterServerObj = new MasterServerImpl(dir,
-					metaData, repServers);
-			MasterServerClientInterface masterServerStub = (MasterServerClientInterface) UnicastRemoteObject
-					.exportObject(masterServerObj, 0);
-			registry.bind("masterServer", masterServerStub);
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
+		private BufferedReader reader;
+		private boolean isErrorStream;
 
+		public StreamReader(InputStream is, boolean isErrorStream) {
+			this.reader = new BufferedReader(new InputStreamReader(is));
+			this.isErrorStream = isErrorStream;
 		}
 
+		public void run() {
+			try {
+				String line = reader.readLine();
+				while (line != null) {
+					if (isErrorStream)
+						System.err.println(line);
+					else
+						System.out.println(line);
+					line = reader.readLine();
+				}
+				reader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	// arguments: ip-address port dir
+	public static void main(String[] args) throws FileNotFoundException {
+		String masterName = "masterServer";
+		String masterAdd = args[0];
+		int masterPort = new Integer(args[1]);
+		String dir = args[2];
+		File metaData = new File("metaData.txt");
+		File repServers = new File("repServers.txt");
+		TreeMap<String, ReplicaLoc> nameToLocMap = new TreeMap<String, ReplicaLoc>();
+
+		Scanner scanner = new Scanner(repServers);
+
+		while (scanner.hasNext()) {
+			String repName = scanner.next(), repAddress = scanner.next();
+			int repPort = scanner.nextInt();
+			nameToLocMap.put(repName, new ReplicaLoc(repAddress, repName,
+					repPort));
+		}
+		scanner.close();
+
+		try {
+			Registry registry = null;
+			try {
+				registry = LocateRegistry.createRegistry(masterPort);
+			} catch (Exception e) {
+				registry = LocateRegistry.getRegistry(masterPort);
+			}
+
+			MasterServerImpl masterServerObj = new MasterServerImpl(metaData,
+					nameToLocMap);
+			MasterServerClientInterface masterServerStub = (MasterServerClientInterface) UnicastRemoteObject
+					.exportObject(masterServerObj, 0);
+			registry.bind(masterName, masterServerStub);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// pkill -f 'java.*ReplicaServerImpl'
+		try {
+			for (ReplicaLoc repLoc : nameToLocMap.values()) {
+				Process p = Runtime.getRuntime()
+						.exec("ssh " + repLoc.getHost());
+				PrintStream out = new PrintStream(p.getOutputStream(), true);
+				StreamReader ls = new StreamReader(p.getInputStream(), false);
+				StreamReader es = new StreamReader(p.getErrorStream(), true);
+
+				Thread t = new Thread(ls);
+				Thread t2 = new Thread(es);
+				t.start();
+				t2.start();
+
+				out.println("cd workspace/ReplicatedFS/src");
+				out.println("javac API/ReplicaServerClientInterface.java Impl/ReplicaServerImpl.java");
+				// args: name dir port masterName masterAddress masterPort
+				out.println("java  Impl/ReplicaServerImpl " + repLoc.getName()
+						+ " " + dir + " " + repLoc.getPort() + " " + masterName
+						+ " " + masterAdd + " " + masterPort);
+				out.println("exit");
+
+				// t.join();
+				// t2.join();
+			}
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 	}
 }
--- src/Impl/ReplicaServerImpl.java
+++ src/Impl/ReplicaServerImpl.java
@@ -5,21 +5,31 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+import API.MasterServerClientInterface;
 import API.ReplicaServerClientInterface;
 
 public class ReplicaServerImpl implements ReplicaServerClientInterface {
 
-	ConcurrentHashMap<Long, String> fileNameTransaction;
-	ConcurrentHashMap<String, Lock> fileLock;
-	ConcurrentHashMap<String, FileContent> cache;
+	private ConcurrentHashMap<Long, String> fileNameTransaction;
+	private ConcurrentHashMap<String, Lock> fileLock;
+	private ConcurrentHashMap<String, FileContent> cache;
+	private MasterServerClientInterface masterServer;
+	private String dir;
+	private String name;
 
-	public ReplicaServerImpl() {
+	public ReplicaServerImpl(String dir, String name,
+			MasterServerClientInterface masterServer) {
 		fileNameTransaction = new ConcurrentHashMap<>();
 		fileLock = new ConcurrentHashMap<>();
+		this.dir = dir;
+		this.name = name;
 	}
 
 	@Override
@@ -52,10 +62,36 @@ public class ReplicaServerImpl implements ReplicaServerClientInterface {
 		if (fileNameTransaction.contains(txnID)) {
 			String fileName = fileNameTransaction.remove(txnID);
 			FileContent content = cache.remove(fileName);
-			boolean success = true;
+			ReplicaLoc[] locations = null;
 			try {
-				FileWriter fw = new FileWriter(fileName, true);
+				locations = masterServer.read(fileName);
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+			boolean success = true;
+
+			// check if this is the master replica
+			if (locations[0].getName().equals(name)) {
+				for (int i = 1; i < locations.length; i++) {
+					ReplicaLoc loc = locations[i];
+					Registry reg = LocateRegistry.getRegistry(loc.getHost(),
+							loc.getPort());
+					try {
+						ReplicaServerClientInterface repServer = (ReplicaServerClientInterface) reg
+								.lookup(loc.getName());
+						repServer.write(txnID, 1, content);
+						success |= repServer.commit(txnID, 1);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+
+			try {
+				File f = new File(dir + "/" + fileName);
+				FileWriter fw = new FileWriter(f, true);
 				fw.write(content.getData());
+				fw.close();
 			} catch (IOException e) {
 				success = false;
 			}
@@ -74,5 +110,49 @@ public class ReplicaServerImpl implements ReplicaServerClientInterface {
 			return true;
 		}
 		return false;
+	}
+
+	// Arguments: name dir port masterName masterAddress masterPort
+	public static void main(String[] args) {
+		String name = args[0];
+		String dir = args[1];
+		int port = new Integer(args[2]);
+		String masterServerName = args[3];
+		String masterServerAdd = args[4];
+		int masterServerPort = new Integer(args[5]);
+
+		try {
+			File dir1 = new File(dir);
+			if (!dir1.exists())
+				dir1.mkdir();
+			File subDir = new File(dir + "/" + name);
+			if (!subDir.exists())
+				subDir.mkdir();
+			Registry registry = null;
+			try {
+				registry = LocateRegistry.createRegistry(port);
+				System.out.println("hasdaj");
+			} catch (Exception e) {
+				registry = LocateRegistry.getRegistry(port);
+			}
+
+			Registry reg2 = LocateRegistry.getRegistry(masterServerAdd,
+					masterServerPort);
+			MasterServerClientInterface masterServer = (MasterServerClientInterface) reg2
+					.lookup(masterServerName);
+
+			ReplicaServerImpl replicaServerObj = new ReplicaServerImpl(dir
+					+ "/" + name, name, masterServer);
+			ReplicaServerClientInterface replicaServerStub = (ReplicaServerClientInterface) UnicastRemoteObject
+					.exportObject(replicaServerObj, 0);
+			System.out.println("here!!");
+			registry.bind(name, replicaServerStub);
+
+			System.out.println("----" + masterServer.read("file1.txt").length);
+
+			System.out.println("here also!!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
